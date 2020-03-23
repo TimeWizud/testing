2ce21e20785ab2460024733b6355f93f0874b6bb
TimeWizud <mcadney@gmail.com>
2020-03-22 23:00:53 -0700

    Adding a line from my Mac

--- Testing.R
+++ Testing.R
@@ -1,3 +1,5 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+print("It can be editted from either my Mac or PC, as well as anyone with access to the master")
